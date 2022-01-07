74c19f3ecc7b9de324974c1b5b3b9e2727c25ade
shashwat-code <textshashwat@gmail.com>
2022-01-07 23:02:17 +0530

    Initial Commit

---
+++ recipeMaker/recipeMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeMakerApp.swift
+//  recipeMaker
+//
+//  Created by Shashwat . on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct recipeMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
